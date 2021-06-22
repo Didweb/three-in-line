3a16bba741577fb7bb4d1d176e5a859c9532b85e
Didweb <info@did-web.com>
2021-06-22 11:47:32 +0200

    Testing Board Create, Save and Reading

---
+++ tests/Domain/BoardTest.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Test\Domain;
+
+
+use App\Domain\Model\Board;
+use App\Domain\Repository\BoardRepository;
+
+use App\Tests\Double\Persistence\Memory\InFileBoardRepositoryStub;
+use PHPUnit\Framework\TestCase;
+
+final class BoardTest extends TestCase
+{
+
+    private BoardRepository $repository;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->repository = InFileBoardRepositoryStub::repository();
+
+    }
+
+    public function test_should_create_board_3_columns(): void
+    {
+        $board = $this->repository->getBoardData();
+        $board->buildBoard(2);
+        $this->repository->save($board);
+        $countColumns = count($board->cells());
+
+        $this->assertEquals(3, $countColumns);
+    }
+
+    public function test_should_create_board_7_columns(): void
+    {
+        $board = $this->repository->getBoardData();
+        $board->buildBoard(6);
+        $this->repository->save($board);
+        $countColumns = count($board->cells());
+
+        $this->assertEquals(7, $countColumns);
+    }
+}
---
+++ tests/Double/Persistence/Memory/InFileBoardRepositoryStub.php
@@ -0,0 +1,64 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Double\Persistence\Memory;
+
+
+use App\Domain\Model\Board;
+use App\Domain\Repository\BoardRepository;
+use Symfony\Component\Finder\Finder;
+use Symfony\Component\Yaml\Yaml;
+
+final class InFileBoardRepositoryStub
+{
+
+    public static function repository(): BoardRepository
+    {
+        return (
+        new class() implements BoardRepository
+        {
+            const PATH_DIR_SOURCES = __DIR__.'/Sources/';
+            const PATH_SOURCES = __DIR__.'/Sources/Board.yml';
+
+
+            public function getBoardData(): Board
+            {
+                $result = [];
+                $this->createFileData();
+
+                $finder = new Finder();
+                $finder->files()->in(self::PATH_DIR_SOURCES);
+                if ($finder->hasResults()) {
+                    foreach ($finder as $file) {
+                        $config = Yaml::parseFile($file->getRealPath());
+                        $result['board'] = reset($config);
+                    }
+                }
+
+                return new Board(
+                    $result['board']['gameIsActive'],
+                    $result['board']['rows'],
+                    $result['board']['columns'],
+                    $result['board']['cells'],
+                );
+            }
+
+            public function save(Board $board): void
+            {
+                $yaml = Yaml::dump($board->toArray());
+                file_put_contents(self::PATH_SOURCES, $yaml);
+            }
+
+            public function createFileData(): void
+            {
+                if (!file_exists(self::PATH_SOURCES)) {
+                    $board = new Board();
+                    $yaml = Yaml::dump($board->toArray());
+                    file_put_contents(self::PATH_SOURCES, $yaml);
+                }
+            }
+        }
+        );
+    }
+}
